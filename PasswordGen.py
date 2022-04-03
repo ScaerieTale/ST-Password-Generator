77f96a1c9b874952c812025e4c9ac8fa72f2fe59
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-02 21:50:23 -0500

    Initial commit setup
    Setting up the boilerplate.  The code in this version is provided courtesy Dr. Angela Yu of the London App Brewery for instructional purposes.  The rest of these commits will be my own code :)

--- PasswordGen.py
+++ PasswordGen.py
@@ -1,8 +1,3 @@
-'''This code was written in part under the guidance of Dr. Angela Yu of the London App Brewery
-and as such is 100% CC Zero.  I hope you're able to find it as useful as I did learning to 
-write it! ♥
--- ScaerieTale'''
-
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
