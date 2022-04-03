a259b4b02204fef50fd95d364b40b40a5e87cd96
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-02 22:09:54 -0500

    Phase 2 finished!
    I'll create a release file in just a second, but, that was amazingly easy (thanks Stack Overflow!)  It now not only takees the data and generates a password, it then takes that password and scrambles it, making it truly random

--- PasswordGen.py
+++ PasswordGen.py
@@ -4,6 +4,7 @@ write it! ♥
 -- ScaerieTale'''
 
 import random
+import sys
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -23,3 +24,4 @@ for sym in range(1, nr_symbols):
 for nums in range(1, nr_numbers):
     password = password + random.choice(numbers)
 print(password)
+print(f"Your password: {''.join(random.sample(password,len(password)))}")
