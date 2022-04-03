c2258112b49e07b1b556ef882121351c6072f72d
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-02 21:56:12 -0500

    Phase 1 update!
    So it took me a hot minute to work out how to get the password string to actually generate (I was being blonde haha).  Next I need to google around and learn how to randomize the order of the characters.  I have ideas, but we'll see... :)

--- PasswordGen.py
+++ PasswordGen.py
@@ -1,3 +1,8 @@
+'''This code was written in part under the guidance of Dr. Angela Yu of the London App Brewery
+and as such is 100% CC Zero.  I hope you're able to find it as useful as I did learning to 
+write it! ♥
+-- ScaerieTale'''
+
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -8,3 +13,13 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# ScaerieTale's code #
+
+password = ""
+for char in range(1, nr_letters):
+    password = password + random.choice(letters)
+for sym in range(1, nr_symbols):
+    password = password + random.choice(symbols)
+for nums in range(1, nr_numbers):
+    password = password + random.choice(numbers)
+print(password)
