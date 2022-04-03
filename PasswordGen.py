6d444db5523a78860215ac6dd7e4ffc6ec8a18e7
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-02 21:42:15 -0500

    Initial commit

---
+++ PasswordGen.py
@@ -0,0 +1,15 @@
+'''This code was written in part under the guidance of Dr. Angela Yu of the London App Brewery
+and as such is 100% CC Zero.  I hope you're able to find it as useful as I did learning to 
+write it! ♥
+-- ScaerieTale'''
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
